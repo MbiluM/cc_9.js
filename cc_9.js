955fc7bf9cb57b9acf6f99d67ca28666a1d77c4c
MbiluM <mabilu@usf.edu>
2025-02-20 23:28:14 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -21,3 +21,22 @@ console.log(emp1.calculateAnnualSalary());
 // Expected output: 60000
 
 
+// Task 2 - Created Manager Class with Inheritance
+class Manager extends Employee {
+    constructor (name, id, department, salary, teamSize) {
+    super(name, id, department, salary)
+    this.teamSize = teamSize
+    };
+    getDetails() {
+    console.log(`Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`);
+    };
+    calculateBonus() {
+    return this.calculateAnnualSalary() * 0.1;
+    };
+}
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+// Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+console.log(mgr1.calculateBonus()); 
+// Expected output: 9600
+
